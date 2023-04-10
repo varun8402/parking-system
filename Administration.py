3f2c46100397d147349a23452afa4716803dc78a
Varun Marwah <61690871+varun002004@users.noreply.github.com>
2023-04-11 01:15:54 +0530

    Update Administration.py

--- Administration.py
+++ Administration.py
@@ -1,36 +1,92 @@
+87% of storage used … You can clean up space or get more storage for Drive, Gmail, and Google Photos.
 #program to access the reports and other observatory options.
 #requires the administrative access using an ID and a password.
 import mysql.connector
-db=mysql.connector.connect(user="root",password="admin",host="localhost",database="parking_system")
+from tabulate import tabulate
+db=mysql.connector.connect(user="root",password="admin",host="localhost",database="parking")
 mycursordb=db.cursor()
 initial_count = 1
 chances = 5
+print("Welcome to Rajiv Chowk Parking Administrative system")
 input_id = str(input("Enter the login id here: "))
-mycursordb.execute("select count(*) from adminlogin where userid = %s",(input_id))
-idcheck = mycursordb.fetchone()
-print(idcheck)
-passcheck=mycursordb.execute("Select userid,password from adminlogin")
-idpass = mycursordb.execute("select count(*) userid,password from adminlogin where userid=(%s) and password = (%s)" , (idcheck,passcheck) )
-
+password=input("Enter the Password")
+mycursordb.execute("select ID from login where ID = '%s'" %(input_id))
+idcheck=mycursordb.fetchall()
+for i in idcheck:
+    c=i[0]
+mycursordb.execute("select Password from login where ID = '%s'" % (input_id))
+passcheck=mycursordb.fetchall()
+for a in passcheck:
+    d=a[0]
+if input_id==c and password==d:
+    Options=int(input("""
+Enter 1:For Tabular Report
+Enter 2:For Revenue collected
+Enter 3:  No. of cars
+Enter 4:  Get Log Files of Specified Date
+Enter 5: Current status of Pillars
+Enter 6: Cars Present Inside Currently
+Enter 7:Frequency Of a Car"""))
+    if Options==1:
+        mycursordb.execute("select VehicleNum,Floor,Pillar,EntryTime,ExitTime,code,AmountPaid from logfiles")
+        result=mycursordb.fetchall()
+        print(tabulate(result,headers=["VehicleNum","Floor","Pillar","EntryTime","ExitTime","Code","Amount"],tablefmt="psql"))
+    if Options==2:
+        mycursordb.execute("select SUM(AmountPaid)from logfiles")
+        total=mycursordb.fetchall()
+        for q in total:
+            print("Total Revenue collected:",q[0])
+    if Options==3:
+        mycursordb.execute("select COUNT(VehicleNum)from logfiles")
+        count=mycursordb.fetchall()
+        for w in count:
+            print("Total Number of Cars:",w[0])
+    if Options==4:
+        date=input("Enter the Date(YYYY/MM/DD)")
+        mycursordb.execute("select VehicleNum,Floor,Pillar,EntryTime,ExitTime,code,AmountPaid from logfiles where cast(EntryTime as date)= '%s'"%(date))
+        res=mycursordb.fetchall()
+        print(tabulate(res,headers=["VehicleNum","Floor","Pillar","EntryTime","ExitTime","Code","Amount"],tablefmt="psql"))
+        
+    if Options==5:
+        floor=int(input("Enter Floor Number"))
+        if floor==1:
+            mycursordb.execute("select *from floor_1")
+            floor_1=mycursordb.fetchall()
+            print(tabulate(floor_1,headers=["Floor","Pillars"],tablefmt="psql"))
+                  
+        if floor==2:
+            mycursordb.execute("select *from floor_2")
+            floor_2=mycursordb.fetchall()
+            print(tabulate(floor_2,headers=["Floor","Pillars"],tablefmt="psql"))
+                  
+        if floor==3:
+            mycursordb.execute("select *from floor_3")
+            floor_3=mycursordb.fetchall()
+            print(tabulate(floor_3,headers=["Floor","Pillars"],tablefmt="psql"))
+        if floor>3:
+            print("Invalid Input")
+            
+    if Options==6:
+        mycursordb.execute("select COUNT(VehicleNum) from logfiles where AmountPaid=0")
+        count1=mycursordb.fetchall()
+        for z in count1:
+            print("Total Number of Cars in Parking are:",z[0])
+            
+    if Options==7:
+        Car=input("Enter Vehicle Registration Number")
+        mycursordb.execute("select COUNT(VehicleNum) from logfiles where VehicleNum= '%s'" % (Car))
+        count2=mycursordb.fetchall()
+        for xz in count2:
+            print("Frequency Of Car is :",xz[0])
+      
+else:
+    print("Incorrect UserID or Password")
 if input_id in idcheck :
     input_pass = input("Enter the password here: ")
-while initial_count != chances:
-    if idpass == 0:
-        print("ID and Password do not match try again")
-        initial_count += 1
 
-options = ["1: Tabular Report","2: Revenue collected","3: No. of cars","4: Get Log Files of Specified Date","5: Current status of Pillars","6: Cars Present Inside Currently"]
-while initial_count != chances:
-    try:
-        print("Welcome to the Rajiv Chowk car parking administrative system...")
-        print("Options available are:")
-        for i in options:
-            print(i)
-            print("Enter the respective serial number against the option you wish to avail-")
-            admin_preference = int(input("Enter the serial number here: "))
-                
-            break
-    except ValueError:
-        print("Incorrect input, no such option available...!")
+db.close()
+
+
+
 
        
